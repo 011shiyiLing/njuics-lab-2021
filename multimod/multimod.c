7362897308366d1dbfd1812117595f74647283b1
tracer-nju <tracer@nju.edu.cn>
2022-10-10 23:02:14 +0800

    Static hostname: shiyiling-NBLK-WAX9X        Icon name: computer-laptop          Chassis: laptop       Machine ID: fc555b04b49048fe96a26e2f5cee10dd          Boot ID: 9f668fdcd716428c8466bd592c14da26 Operating System: Ubuntu 22.04.1 LTS           Kernel: Linux 5.15.0-48-generic     Architecture: x86-64  Hardware Vendor: HONOR   Hardware Model: NBLK-WAX9X  23:02:14 up  5:41,  1 user,  load average: 0.01, 0.05, 0.04

--- multimod/multimod.c
+++ multimod/multimod.c
@@ -12,7 +12,7 @@ uint64_t mod(uint64_t x,uint64_t m)
 }
 
 //把a转换成二进制字符数组
-void turn_bianry(uint64_t a)
+void turn_binary(uint64_t a)
 {
   uint64_t temp;
   while(a != 0)
@@ -33,6 +33,7 @@ void turn_bianry(uint64_t a)
 uint64_t multimod(uint64_t a, uint64_t b, uint64_t m) 
 {
   uint64_t x=0;
+  turn_binary;
   for(int j=i-1;j>=0;j--)
   {
     if(arr[j] == 1)
