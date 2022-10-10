f91593227c77749afabab5ac3a95dae47222598f
tracer-nju <tracer@nju.edu.cn>
2022-10-10 22:45:28 +0800

    Static hostname: shiyiling-NBLK-WAX9X        Icon name: computer-laptop          Chassis: laptop       Machine ID: fc555b04b49048fe96a26e2f5cee10dd          Boot ID: 9f668fdcd716428c8466bd592c14da26 Operating System: Ubuntu 22.04.1 LTS           Kernel: Linux 5.15.0-48-generic     Architecture: x86-64  Hardware Vendor: HONOR   Hardware Model: NBLK-WAX9X  22:45:28 up  5:24,  1 user,  load average: 0.08, 0.06, 0.06

--- multimod/multimod.c
+++ multimod/multimod.c
@@ -4,25 +4,35 @@
 uint64_t mod(uint64_t x,uint64_t m)
 {
   uint32_t res=0;
-  res = x & (m-1);
+  res = x & (m-1);//要求m必须是2的正整数幂次
   return res;
 }
-
-//multiply operation
-uint64_t multiply(uint64_t a,uint64_t b)
+static int arr[100];
+static int i=0;
+//把a转换成二进制字符数组
+void turn_bianry(uint64_t a)
 {
-  uint32_t res =0;
-  for(int i=b;i>0;i--)
+  uint64_t temp;
+  while(a != 0)
   {
-    res += a;
+    temp = mod(a,2);
+    a = a>>1;
+    arr[i] = temp;
+    i++;
   }
-  return res;
+  return;
 }
 
 uint64_t multimod(uint64_t a, uint64_t b, uint64_t m) {
   uint64_t x=0;
-  x = multiply(a,b); 
+  for(int j=i-1;j>=0;j--)
+  {
+    if(a[j] == 1)
+    {
+      x += b<<j;
+    }
+  }
   uint64_t res = 0;
-  res = mod(x,m);  
+  res = mod(x,m);
   return res; 
 }
