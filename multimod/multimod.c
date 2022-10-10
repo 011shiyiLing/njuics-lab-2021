3e91f3a5336b26fdd4532abde817ec8101d2e0c6
tracer-nju <tracer@nju.edu.cn>
2022-10-10 23:14:43 +0800

    Static hostname: shiyiling-NBLK-WAX9X        Icon name: computer-laptop          Chassis: laptop       Machine ID: fc555b04b49048fe96a26e2f5cee10dd          Boot ID: 9f668fdcd716428c8466bd592c14da26 Operating System: Ubuntu 22.04.1 LTS           Kernel: Linux 5.15.0-48-generic     Architecture: x86-64  Hardware Vendor: HONOR   Hardware Model: NBLK-WAX9X  23:14:43 up  5:53,  1 user,  load average: 0.08, 0.10, 0.06

--- multimod/multimod.c
+++ multimod/multimod.c
@@ -1,8 +1,7 @@
 #include <stdint.h>
 #include <stdio.h>
 
-static int i =0;
-static int arr[100];
+static int i;
 //mod operation
 uint64_t mod(uint64_t x,uint64_t m)
 {
@@ -14,6 +13,7 @@ uint64_t mod(uint64_t x,uint64_t m)
 //把a转换成二进制字符数组
 void turn_binary(uint64_t a)
 {
+  i = 0;
   uint64_t temp;
   while(a != 0)
   {
@@ -27,6 +27,7 @@ void turn_binary(uint64_t a)
 
 uint64_t multimod(uint64_t a, uint64_t b, uint64_t m) 
 {
+  int arr[100];
   uint64_t x=0;
   turn_binary(a);
   for(int j=i-1;j>=0;j--)
