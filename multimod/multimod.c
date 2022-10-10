09d04b0eed3f505f0f0e3c13676253c0e3043a16
tracer-nju <tracer@nju.edu.cn>
2022-10-10 18:25:11 +0800

    Static hostname: shiyiling-NBLK-WAX9X        Icon name: computer-laptop          Chassis: laptop       Machine ID: fc555b04b49048fe96a26e2f5cee10dd          Boot ID: 9f668fdcd716428c8466bd592c14da26 Operating System: Ubuntu 22.04.1 LTS           Kernel: Linux 5.15.0-48-generic     Architecture: x86-64  Hardware Vendor: HONOR   Hardware Model: NBLK-WAX9X  18:25:11 up  1:04,  1 user,  load average: 0.33, 0.50, 0.48

--- multimod/multimod.c
+++ multimod/multimod.c
@@ -1,4 +1,5 @@
 #include <stdint.h>
+#include <stdio.h>
 
 //mod operation
 uint64_t mod(uint64_t x,uint64_t m)
@@ -22,7 +23,7 @@ uint64_t multiply(uint64_t a,uint64_t b)
 uint64_t multimod(uint64_t a, uint64_t b, uint64_t m) {
   uint64_t x=0;
   x = multiply(a,b); 
-  printf("%u\n",x);
+  printf("%lu\n",x);
   uint64_t res = 0;
   res = mod(x,m);  
   return res; 
