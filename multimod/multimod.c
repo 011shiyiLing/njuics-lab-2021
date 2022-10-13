8d152c53ba71115212604a0e3af46299bc538c68
tracer-nju <tracer@nju.edu.cn>
2022-10-13 19:17:48 +0800

    Static hostname: shiyiling-NBLK-WAX9X        Icon name: computer-laptop          Chassis: laptop       Machine ID: fc555b04b49048fe96a26e2f5cee10dd          Boot ID: 0c975f95e2f24bf9b1c50f68ec907093 Operating System: Ubuntu 22.04.1 LTS           Kernel: Linux 5.15.0-48-generic     Architecture: x86-64  Hardware Vendor: HONOR   Hardware Model: NBLK-WAX9X  19:17:48 up  1:37,  1 user,  load average: 0.31, 0.25, 0.33

--- multimod/multimod.c
+++ multimod/multimod.c
@@ -6,7 +6,7 @@ uint64_t modd(uint64_t a,uint64_t b)
   if (b > a) return a;
   else if (b==a) return 0;
 
-  else if(b >= (18446744073709551614>>4))
+  else if(b >= (57646075230340000))
   {
     printf("%d\n",1);
     while(a >= b)
