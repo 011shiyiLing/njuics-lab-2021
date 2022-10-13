01b8663e651f94b8fdae7be77cde02a05d6a673b
tracer-nju <tracer@nju.edu.cn>
2022-10-13 18:32:09 +0800

    Static hostname: shiyiling-NBLK-WAX9X        Icon name: computer-laptop          Chassis: laptop       Machine ID: fc555b04b49048fe96a26e2f5cee10dd          Boot ID: 0c975f95e2f24bf9b1c50f68ec907093 Operating System: Ubuntu 22.04.1 LTS           Kernel: Linux 5.15.0-48-generic     Architecture: x86-64  Hardware Vendor: HONOR   Hardware Model: NBLK-WAX9X  18:32:09 up 51 min,  1 user,  load average: 0.20, 0.31, 0.27

--- multimod/multimod.c
+++ multimod/multimod.c
@@ -1,20 +1,18 @@
 #include <stdint.h>
-#include <stdio.h>
-
-static int i;
-static uint64_t arr[100];
 
 uint64_t modd(uint64_t a,uint64_t b)
 {
+  if (b > a) return a;
+  else if (b==a) return 0;
   uint64_t c = b;
   while(a >= b)
   {
     while(c < a)
     {
-      c = c << 4;
+      c = c << 2;
       if (c > a)
       {
-        c = c>>4;
+        c = c>>2;
         break;
       }
     }
@@ -42,34 +40,28 @@ uint64_t mod(uint64_t x,uint64_t m)
   
 }
 
-//把a转换成二进制字符数组
-void turn_binary(uint64_t a)
-{
-  uint64_t temp;
-  while(a != 0)
-  {
-    temp = a & 1;
-    a = a>>1;
-    arr[i] = temp;
-    i++;
-  }
-
-}
 
 uint64_t multimod(uint64_t a, uint64_t b, uint64_t m) 
 {
-  uint64_t x=0;
-  i =0;
-  turn_binary(a);
-  for(int j=i-1;j>=0;j--)
-  {
-    if(arr[j] == 1)
-    {
-      x += mod(b<<j,m);
-    }
-  }
-  
+  uint64_t exp = mod(a,m);
   uint64_t res = 0;
-  res = mod(x,m);
-  return res; 
+
+  while(b)
+  {
+    if(b&1)//judge b's lasted position == 1
+    {
+      res = res + exp;
+      if(res >= m)
+      {
+        res = res - m;
+      }
+    }
+    exp = exp << 1;
+    if(exp > m)
+    {
+      exp = exp -m;
+    }
+    b >>= 1;
+  }
+  return res;
 }
