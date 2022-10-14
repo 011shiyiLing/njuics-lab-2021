5b0e0c7231eae99e1f19809c98d730bdc4871ebc
tracer-nju <tracer@nju.edu.cn>
2022-10-14 21:24:56 +0800

    Static hostname: shiyiling-NBLK-WAX9X        Icon name: computer-laptop          Chassis: laptop       Machine ID: fc555b04b49048fe96a26e2f5cee10dd          Boot ID: 83f9d56ac23e4b539ff023bf734c1748 Operating System: Ubuntu 22.04.1 LTS           Kernel: Linux 5.15.0-48-generic     Architecture: x86-64  Hardware Vendor: HONOR   Hardware Model: NBLK-WAX9X  21:24:56 up  2:22,  1 user,  load average: 0.31, 0.29, 0.27

--- multimod/multimod.c
+++ multimod/multimod.c
@@ -1,26 +1,27 @@
 #include <stdint.h>
+//#include <stdio.h>
 
 //2^64-1
 static uint64_t maxmum = -1;
 //2^63
 static uint64_t smaxmum = (uint64_t) 1<<63;
 
-uint64_t modd(uint64_t a,uint64_t m)
+uint64_t moddd(uint64_t a,uint64_t m)
 {
- uint64_t c = m;
- while(a >= m)
- {
-  while(c < smaxmum && c < a)
+  uint64_t c = m;
+  while(a >= m)
   {
-    c = c << 1;
-  }
-  while(c > a && c > m)
-  {
-    c = c >> 1;
-  }
-  a = a - c;
- }  
- return a;
+    while(c < smaxmum && c < a)
+    {
+      c = c << 1;
+    }
+    while(c > a && c > m)
+    {
+      c = c >> 1;
+    }
+    a = a - c;
+  }  
+  return a;
 }
 
 //mod operation
@@ -29,65 +30,41 @@ uint64_t mod(uint64_t x,uint64_t m)
   uint64_t res=0;
   if((m & (m-1)) == 0)//要求m必须是2的正整数幂次
   {
-    res = x & (m-1);
+    //res = x & (m-1);
+    res = moddd(x,m);
     return res;
   }
   
   else
   {
-    res = modd(x,m);
+    res = moddd(x,m);
     return res;
   }
   
 }
 
-//attention:'a+b''s value is out of bound
-//(a+b) mod m = (a mod m + b mod m)mod m
-uint64_t plusmod(uint64_t a,uint64_t b,uint64_t m)
-{
-  uint64_t sum = 0;
-  a = mod(a,m);
-  b = mod(b,m);
-
-  sum = a+b;
-
-  while(a != 0 && b!= 0  && a >= maxmum-b-1 )
-  {
-    a = mod(sum,m);
-    b = mod(maxmum,m) + mod(1,m);
-    sum = a+b;
-  }
-
-  uint64_t res = 0;
-  res = mod(sum,m);
-  return res;
-}
-
-
 uint64_t multimod(uint64_t a, uint64_t b, uint64_t m) 
 {
-  uint64_t res=0;
-  //turn_binary(a);
-  uint64_t j = 1;
-  for(;j<64;j++)
-  {
-    if (((a>>j)&1) == 0) continue;
-    else
-    {
-      int k =0;
-      uint64_t b1 = b;
-      for(;k<j;k++)
-      {
-        b1 = plusmod(b1,b1,m);
-      }
+  uint64_t exp = mod(a,m);
+  uint64_t res = 0;
 
-      res = plusmod(res,b1,m);
-    }
-  }
-  
-  if((a&1) == 1)
+  while(b)
   {
-    res = plusmod(res,b,m);
+    if(b&1)//judge b's lasted position == 1
+    {
+      res = res + exp;
+      if(res >= m)
+      {
+        res = res - m;
+      }
+    }
+    exp = exp << 1;
+    if(exp > m)
+    {
+      exp = exp -m;
+    }
+    b >>= 1;
   }
   return res;
 }
+
