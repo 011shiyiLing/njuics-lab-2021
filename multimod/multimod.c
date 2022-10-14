1a73eb390f4aaf848b5e3bde70c059e02096b73a
tracer-nju <tracer@nju.edu.cn>
2022-10-14 15:43:30 +0800

    Static hostname: shiyiling-NBLK-WAX9X        Icon name: computer-laptop          Chassis: laptop       Machine ID: fc555b04b49048fe96a26e2f5cee10dd          Boot ID: c970da0f5de94fc683fde56d2ffb60d8 Operating System: Ubuntu 22.04.1 LTS           Kernel: Linux 5.15.0-48-generic     Architecture: x86-64  Hardware Vendor: HONOR   Hardware Model: NBLK-WAX9X  15:43:30 up  1:31,  1 user,  load average: 0.45, 0.46, 0.39

--- multimod/multimod.c
+++ multimod/multimod.c
@@ -1,6 +1,7 @@
 #include <stdint.h>
 
-
+static uint64_t arr[64];
+static uint64_t i=0;
 //2^64-1
 static uint64_t maxmum = -1;
 //2^63
@@ -47,7 +48,17 @@ uint64_t mod(uint64_t x,uint64_t m)
   
 }
 
-
+void turn_binary(uint64_t t)
+{
+  uint64_t a = t;
+  while(a != 0)
+  {
+    uint64_t temp = mod(a,2);
+    arr[i] = temp;
+    a = a >> 1;
+    i++;
+  }
+}
 
 //attention:'a+b''s value is out of bound
 //(a+b) mod m = (a mod m + b mod m)mod m
@@ -74,8 +85,28 @@ uint64_t plusmod(uint64_t a,uint64_t b,uint64_t m)
 
 uint64_t multimod(uint64_t a, uint64_t b, uint64_t m) 
 {
-  
-  uint64_t res = plusmod(a,b,m);
-  return res;
-}
+  uint64_t x=0;
+  turn_binary(a);
+  uint64_t j = 1;
+  for(;j<64;j++)
+  {
+    if (bitof(a,j) == 0) continue;
+    else
+    {
+      int k =0;
+      uint64_t b1 = b;
+      for(;k<j;k++)
+      {
+        b1 = plusmod(b1,b1,m);
+      }
 
+      x = plusmod(x,b1,m);
+    }
+  }
+  
+  if(bitof(a,0) == 1)
+  {
+    x = plusmod(x,b,m);
+  }
+  return x;
+}
