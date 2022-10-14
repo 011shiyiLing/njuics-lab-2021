8cf5e02b646fd541faa8645541dc1a1b5240a5f2
tracer-nju <tracer@nju.edu.cn>
2022-10-14 21:15:19 +0800

    Static hostname: shiyiling-NBLK-WAX9X        Icon name: computer-laptop          Chassis: laptop       Machine ID: fc555b04b49048fe96a26e2f5cee10dd          Boot ID: 83f9d56ac23e4b539ff023bf734c1748 Operating System: Ubuntu 22.04.1 LTS           Kernel: Linux 5.15.0-48-generic     Architecture: x86-64  Hardware Vendor: HONOR   Hardware Model: NBLK-WAX9X  21:15:19 up  2:12,  1 user,  load average: 0.46, 0.42, 0.31

--- multimod/multimod.c
+++ multimod/multimod.c
@@ -9,18 +9,18 @@ static uint64_t smaxmum = (uint64_t) 1<<63;
 
 uint64_t modd(uint64_t a,uint64_t m)
 {
- uint64_t m1 = m;
+ uint64_t c = m;
  while(a >= m)
  {
-  while(m1 < smaxmum && m1 < a)
+  while(c < smaxmum && c < a)
   {
-    m1 = m1 << 2;
+    c = c << 2;
   }
-  while(m1 > a && m1 > m)
+  while(c > a && c > m)
   {
-    m1 = m1 >> 2;
+    c = c >> 2;
   }
-  a -= m1;
+  a = a - c;
  }  
  return a;
 }
@@ -48,7 +48,7 @@ void turn_binary(uint64_t t)
   uint64_t a = t;
   while(a != 0)
   {
-    uint64_t temp = mod(a,2);
+    uint64_t temp = a&1;
     arr[i] = temp;
     a = a >> 1;
     i++;
@@ -65,7 +65,7 @@ uint64_t plusmod(uint64_t a,uint64_t b,uint64_t m)
 
   sum = a+b;
 
-  while(a != 0 && b!= 0  && b-1 >= maxmum - a)
+  while(a != 0 && b!= 0  && a >= maxmum-b-1 )
   {
     a = mod(sum,m);
     b = mod(maxmum,m) + mod(1,m);
@@ -80,7 +80,7 @@ uint64_t plusmod(uint64_t a,uint64_t b,uint64_t m)
 
 uint64_t multimod(uint64_t a, uint64_t b, uint64_t m) 
 {
-  uint64_t x=0;
+  uint64_t res=0;
   turn_binary(a);
   uint64_t j = 1;
   for(;j<64;j++)
@@ -95,13 +95,13 @@ uint64_t multimod(uint64_t a, uint64_t b, uint64_t m)
         b1 = plusmod(b1,b1,m);
       }
 
-      x = plusmod(x,b1,m);
+      res = plusmod(res,b1,m);
     }
   }
   
   if((a&1) == 1)
   {
-    x = plusmod(x,b,m);
+    res = plusmod(res,b,m);
   }
-  return x;
+  return res;
 }
