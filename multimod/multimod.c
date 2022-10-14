976cac58bfc5b2745060ccd8e9248bff0c9a236c
tracer-nju <tracer@nju.edu.cn>
2022-10-14 15:04:56 +0800

    Static hostname: shiyiling-NBLK-WAX9X        Icon name: computer-laptop          Chassis: laptop       Machine ID: fc555b04b49048fe96a26e2f5cee10dd          Boot ID: c970da0f5de94fc683fde56d2ffb60d8 Operating System: Ubuntu 22.04.1 LTS           Kernel: Linux 5.15.0-48-generic     Architecture: x86-64  Hardware Vendor: HONOR   Hardware Model: NBLK-WAX9X  15:04:56 up 52 min,  1 user,  load average: 0.29, 0.21, 0.22

--- multimod/multimod.c
+++ multimod/multimod.c
@@ -43,6 +43,28 @@ uint64_t mod(uint64_t x,uint64_t m)
   
 }
 
+//attention:'a+b' is out of bound
+//(a+b) mod m = (a mod m + b mod m)mod m
+uint64_t plusmod(uint64_t a,uint64_t b,uint64_t m)
+{
+  uint64_t sum = 0;
+  a = mod(a,m);
+  b = mod(b,m);
+
+  sum = a+b;
+
+  while(a != 0 && b!= 0  && b-1 >= maxmum - a)
+  {
+    a = mod(sum,m);
+    b = mod(maxmum,m) + mod(1,m);
+    sum = a+b;
+  }
+
+  uint32_t res = 0;
+  res = mod(sum,m);
+  return res;
+}
+
 
 //把a转换成二进制字符数组
 void turn_binary(uint64_t a)
@@ -55,23 +77,22 @@ void turn_binary(uint64_t a)
     arr[i] = temp;
     i++;
   }
-
 }
 
 uint64_t multimod(uint64_t a, uint64_t b, uint64_t m) 
 {
-  uint64_t x=0;
-  i =0;
-  turn_binary(a);
-  for(int j=i-1;j>=0;j--)
-  {
-    if(arr[j] == 1)
-    {
-      x += b<<j;
-    }
-  }
+  //uint64_t x=0;
+  //i =0;
+  //turn_binary(a);
+  //for(int j=i-1;j>=0;j--)
+  //{
+    //if(arr[j] == 1)
+    //{
+      //x += b<<j;
+    //}
+    //}
   
   uint64_t res = 0;
-  res = mod(x,m);
+  res = plusmod(a,b,m);
   return res; 
 }
