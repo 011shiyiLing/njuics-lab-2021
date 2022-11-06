a1cf04d4d81ef7087110ad2166b44c1694bfe8ca
tracer-nju <tracer@nju.edu.cn>
2022-11-06 15:53:38 +0800

    Static hostname: shiyiling-NBLK-WAX9X        Icon name: computer-laptop          Chassis: laptop       Machine ID: fc555b04b49048fe96a26e2f5cee10dd          Boot ID: 2b9665863df340988d8d8fd05bc63fff Operating System: Ubuntu 22.04.1 LTS           Kernel: Linux 5.15.0-48-generic     Architecture: x86-64  Hardware Vendor: HONOR   Hardware Model: NBLK-WAX9X  15:53:38 up  4:57,  1 user,  load average: 1.11, 0.58, 0.39

--- asm/main.c
+++ asm/main.c
@@ -3,9 +3,9 @@
 #include <stdio.h>
 
 int main() {
+  assert(asm_add(1,1) == 2);
   asm_jmp_buf buf;
-  //int r = asm_setjmp(buf);
-  int r = 0;
+  int r = asm_setjmp(buf);
   if (r == 0) {
     assert(asm_add(1234, 5678) == 6912);
     assert(asm_popcnt(0x0123456789abcdefULL) == 32);
