3cb7c29e8918df7bcf076adf39c1b92a0e931fd7
tracer-nju <tracer@nju.edu.cn>
2022-11-07 18:58:32 +0800

    Static hostname: shiyiling-NBLK-WAX9X        Icon name: computer-laptop          Chassis: laptop       Machine ID: fc555b04b49048fe96a26e2f5cee10dd          Boot ID: 77ca025a98b94eb992b6984e8229dfb1 Operating System: Ubuntu 22.04.1 LTS           Kernel: Linux 5.15.0-48-generic     Architecture: x86-64  Hardware Vendor: HONOR   Hardware Model: NBLK-WAX9X  18:58:32 up  9:29,  1 user,  load average: 0.35, 0.37, 0.43

--- asm/asm-impl.c
+++ asm/asm-impl.c
@@ -20,12 +20,11 @@ int asm_popcnt(uint64_t x) {
          "movl $0,-8(%rbp);"
          "movl $0,-4(%rbp);"
          "jmp .L2;"
-    ".L1: movl -4(%rbp),%eax;"
-         "movq %rdi,%rdx;"
-         "movl %eax,%ecx;"
+    ".L1: movq %rdi,%rdx;"
+         "movl -4(%rbp),%ecx;"
          "shrq %cl,%rdx;"
          "movq %rdx,%rax;"
-         "andl $1,%eax;"
+         //"andl $1,%eax;"
          "testq %rax,%rax;"
          "je .L3;"
          "addl $1,-8(%rbp);"
@@ -39,11 +38,11 @@ int asm_popcnt(uint64_t x) {
   return 0;
 }
 
-/*void *asm_memcpy(void *dest, const void *src, size_t n) {
+void *asm_memcpy(void *dest, const void *src, size_t n) {
   return memcpy(dest, src, n);
 }
 
-int asm_setjmp(asm_jmp_buf env) {
+/*int asm_setjmp(asm_jmp_buf env) {
   return setjmp(env);
 }
 
