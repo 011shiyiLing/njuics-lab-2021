7115f018e6de8670aa2496d008b75c2ec719be53
tracer-nju <tracer@nju.edu.cn>
2022-11-07 18:02:23 +0800

    Static hostname: shiyiling-NBLK-WAX9X        Icon name: computer-laptop          Chassis: laptop       Machine ID: fc555b04b49048fe96a26e2f5cee10dd          Boot ID: 77ca025a98b94eb992b6984e8229dfb1 Operating System: Ubuntu 22.04.1 LTS           Kernel: Linux 5.15.0-48-generic     Architecture: x86-64  Hardware Vendor: HONOR   Hardware Model: NBLK-WAX9X  18:02:23 up  8:33,  1 user,  load average: 0.52, 0.31, 0.20

--- asm/asm-impl.c
+++ asm/asm-impl.c
@@ -6,21 +6,47 @@ int64_t asm_add(int64_t a, int64_t b) {
   asm(
     "lea (%rdi,%rsi,1),%eax;"
     "retq;"
-    //: "=a"(c)//output
-    //: "d"(a),"s"(b)//input
   );
   return 0;
 }
 
 int asm_popcnt(uint64_t x) {
-  int s = 0;
+  /*int s = 0;
   for (int i = 0; i < 64; i++) {
     if ((x >> i) & 1) s++;
-  }
-  return s;
+  }*/
+  asm(
+    "pushq %rbp;"
+    "movl %rdi,-24(%rbp);"
+    "movl $0,-8(%rbp);"
+    "movl $0, -4(%rbp);"
+    "jmp .L2;"
+
+    ".L1:"
+    "movl -4(%rbp),%eax;"
+    "movq -24(%rbp),%rdx;"
+    "movl %eax,%ecx;"
+    "shrq %cl,%rdx;"
+    "movq %rdx,%rax;"
+    "andl $1,%eax;"
+    "testq %rax,%rax;"
+    "je .L3;"
+    "addl $1,-8(%rbp);"
+
+    ".L2:"
+    "cmpl $63,-4(%rbp);"
+    "jle .L1;"
+    "movl -8(%rbp),%eax;"
+    "popq %rbp;"
+    "ret;"
+
+    ".L3:"
+    "addl $1,-4(%rbp);"
+  );
+  return 0;
 }
 
-void *asm_memcpy(void *dest, const void *src, size_t n) {
+/*void *asm_memcpy(void *dest, const void *src, size_t n) {
   return memcpy(dest, src, n);
 }
 
@@ -30,4 +56,4 @@ int asm_setjmp(asm_jmp_buf env) {
 
 void asm_longjmp(asm_jmp_buf env, int val) {
   longjmp(env, val);
-}
+}*/
