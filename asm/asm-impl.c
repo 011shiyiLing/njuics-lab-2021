a13e26af6d40e7110117d713547736fcc3dec8ea
tracer-nju <tracer@nju.edu.cn>
2022-11-07 21:49:24 +0800

    Static hostname: shiyiling-NBLK-WAX9X        Icon name: computer-laptop          Chassis: laptop       Machine ID: fc555b04b49048fe96a26e2f5cee10dd          Boot ID: 77ca025a98b94eb992b6984e8229dfb1 Operating System: Ubuntu 22.04.1 LTS           Kernel: Linux 5.15.0-48-generic     Architecture: x86-64  Hardware Vendor: HONOR   Hardware Model: NBLK-WAX9X  21:49:24 up 12:20,  1 user,  load average: 0.43, 0.22, 0.20

--- asm/asm-impl.c
+++ asm/asm-impl.c
@@ -45,7 +45,7 @@ void *asm_memcpy(void *dest, const void *src, size_t n) {
     "shr $2, %2;"
     "cld;" //set DF = 0
     "rep; movsl;"
-    "mov %3, %%ecx;"
+    "movl %3, %%ecx;"
     "and $3, %%ecx;" //对%cx取余
     "rep; movsb;"
     : //output
