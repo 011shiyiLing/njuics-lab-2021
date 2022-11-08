6303af71a0f6ced80532934e759aa3d6a800fd18
tracer-nju <tracer@nju.edu.cn>
2022-11-08 21:22:12 +0800

    Static hostname: shiyiling-NBLK-WAX9X        Icon name: computer-laptop          Chassis: laptop       Machine ID: fc555b04b49048fe96a26e2f5cee10dd          Boot ID: 8c86ebd32664438ea2c7670784f9eb68 Operating System: Ubuntu 22.04.1 LTS           Kernel: Linux 5.15.0-48-generic     Architecture: x86-64  Hardware Vendor: HONOR   Hardware Model: NBLK-WAX9X  21:22:12 up  7:27,  1 user,  load average: 0.29, 0.26, 0.23

--- asm/asm-impl.c
+++ asm/asm-impl.c
@@ -22,10 +22,9 @@ int asm_popcnt(uint64_t x) {
          "jmp .L2;" //no condition jump
     ".L1: movq %rdi,%rdx;"
          "movl -4(%rbp),%ecx;"
-         "shrq %cl,%rdx;"
+         "shrq %ecx,%rdx;"
          "andq $1,%rdx;"
          "cmp $0,%rdx;"
-         //"testq %rdx,%rdx;" //是0，ZF位置1;不是0，ZF位置0
          "je .L3;" //when ZF=0 jump
          "addl $1,-8(%rbp);" //s++
     ".L3: addl $1,-4(%rbp);" //i++
