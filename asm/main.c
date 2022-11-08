73e0e4f16cdd6fa416c760d383e81d10f1d6feac
tracer-nju <tracer@nju.edu.cn>
2022-11-08 14:18:57 +0800

    Static hostname: shiyiling-NBLK-WAX9X        Icon name: computer-laptop          Chassis: laptop       Machine ID: fc555b04b49048fe96a26e2f5cee10dd          Boot ID: 8c86ebd32664438ea2c7670784f9eb68 Operating System: Ubuntu 22.04.1 LTS           Kernel: Linux 5.15.0-48-generic     Architecture: x86-64  Hardware Vendor: HONOR   Hardware Model: NBLK-WAX9X  14:18:57 up 23 min,  1 user,  load average: 1.36, 1.32, 1.03

--- asm/main.c
+++ asm/main.c
@@ -3,16 +3,16 @@
 #include <stdio.h>
 
 int main() {
-  /*if(asm_add(1,1) == 2) printf("PASSED.\n");
+  if(asm_add(1,1) == 2) printf("PASSED.\n");
   if(asm_add(1234,5678) == 6912) printf("PASSED.\n");
   if(asm_popcnt(0x0123456789abcdefULL) == 32) printf("PASSED.\n");
   //test asm_memcpy
   char src[1024] = {"i love ics!"};
   char dest[1024] = {0};
   asm_memcpy(dest,src,sizeof(src)/sizeof(char));
-  printf("dest:%s\n",dest);*/
+  printf("dest:%s\n",dest);
 
-  asm_jmp_buf buf;
+  /*asm_jmp_buf buf;
   int r = asm_setjmp(buf);
   if (r == 0) {
     assert(asm_add(1234, 5678) == 6912);
@@ -22,5 +22,5 @@ int main() {
   } else {
     assert(r == 123);
     printf("PASSED.\n");
-  }
+  }*/
 }
