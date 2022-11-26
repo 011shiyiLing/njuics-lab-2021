56304f963e1fc691ab2e9909dc6a92375aae27d2
tracer-nju <tracer@nju.edu.cn>
2022-11-26 16:49:09 +0800

    Static hostname: shiyiling-NBLK-WAX9X        Icon name: computer-laptop          Chassis: laptop       Machine ID: fc555b04b49048fe96a26e2f5cee10dd          Boot ID: 146f063969914008a92a4e495f1d6269 Operating System: Ubuntu 22.04.1 LTS           Kernel: Linux 5.15.0-53-generic     Architecture: x86-64  Hardware Vendor: HONOR   Hardware Model: NBLK-WAX9X  16:49:09 up  7:42,  1 user,  load average: 0.66, 0.40, 0.31

--- perftune/sieve.c
+++ perftune/sieve.c
@@ -2,20 +2,19 @@
 #include <string.h>
 #include <assert.h>
 #include <stdio.h>
-#include <math.h>
 
 #define N 10000000
 
 static bool is_prime[N];
 static int  primes[N];
 
-int *sieve(int n) {
-  int m = sqrt(n+0.5); 
+int *sieve(int n) { 
   assert(n + 1 < N);
-  for (int i = 0; i <= n; i++)
-    is_prime[i] = true;
+  //for (int i = 0; i <= n; i++)
+    //is_prime[i] = true;
+  memset(is_prime,true,n);
 
-  for (int i = 2; i <= m; i++) {
+  for (int i = 2; i <= n; i++) {
     for (int j = i + i; j <= n; j += i) {
       is_prime[j] = false;
     }
