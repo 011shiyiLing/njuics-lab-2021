1f9c7a3c207e32d2a83b813ee94b8c8308204a46
tracer-nju <tracer@nju.edu.cn>
2022-11-26 17:37:25 +0800

    Static hostname: shiyiling-NBLK-WAX9X        Icon name: computer-laptop          Chassis: laptop       Machine ID: fc555b04b49048fe96a26e2f5cee10dd          Boot ID: 146f063969914008a92a4e495f1d6269 Operating System: Ubuntu 22.04.1 LTS           Kernel: Linux 5.15.0-53-generic     Architecture: x86-64  Hardware Vendor: HONOR   Hardware Model: NBLK-WAX9X  17:37:25 up  8:31,  1 user,  load average: 0.05, 0.12, 0.20

--- perftune/sieve.c
+++ perftune/sieve.c
@@ -35,14 +35,14 @@ int *sieve(int n) {
     //is_prime[i] = true;
   memset(is_prime,true,n);
 
-  for (int i = 2; i <= m; i++) {
+  for (int i = 2; i <= n/i; i++) {
     for (int j = i + i; j <= n; j += i) {
       is_prime[j] = false;
     }
   }
 
   int *p = primes;
-  for (int i = 2; i <= m; i++)
+  for (int i = 2; i <= n; i++)
     if (is_prime[i]) {
       *p++ = i;
     }
