4c97f772dee11d7b34208d27bfcbbebd1f6ff0e5
tracer-nju <tracer@nju.edu.cn>
2022-11-26 17:17:37 +0800

    Static hostname: shiyiling-NBLK-WAX9X        Icon name: computer-laptop          Chassis: laptop       Machine ID: fc555b04b49048fe96a26e2f5cee10dd          Boot ID: 146f063969914008a92a4e495f1d6269 Operating System: Ubuntu 22.04.1 LTS           Kernel: Linux 5.15.0-53-generic     Architecture: x86-64  Hardware Vendor: HONOR   Hardware Model: NBLK-WAX9X  17:17:37 up  8:11,  1 user,  load average: 0.12, 0.24, 0.26

--- perftune/sieve.c
+++ perftune/sieve.c
@@ -7,14 +7,46 @@
 
 static bool is_prime[N];
 static int  primes[N];
+int sqrt(int val)
+{
+  int n;
+  int i;
+  int tmp,ttp;
+
+  if(val == 0) return 0;
+  n = 0;
+  tmp = (val >> 30);
+  val <<= 2;
+  if(tmp > 1){
+    n++;
+    tmp -= n;
+  }
+  for(i=15; i>0; i--)
+  {
+    n <<= 1;
+    tmp <<= 2;
+    tmp += (val >> 30);
+    ttp = n;
+    ttp = (ttp << 1) + 1;
+    val <<= 2;
+    if(tmp >= ttp)
+    {
+      tmp -= ttp;
+      n++;
+    }
+  }
+  return n;
+
+}
 
 int *sieve(int n) { 
   assert(n + 1 < N);
+  int m = sqrt(n+1);
   //for (int i = 0; i <= n; i++)
     //is_prime[i] = true;
   memset(is_prime,true,n);
 
-  for (int i = 2; i <= n; i++) {
+  for (int i = 2; i <= m; i++) {
     for (int j = i + i; j <= n; j += i) {
       is_prime[j] = false;
     }
