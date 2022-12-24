d231fba4b08a705151f2d528f67b03819543009e
tracer-nju <tracer@nju.edu.cn>
2022-12-24 18:41:33 +0800

    Static hostname: shiyiling-NBLK-WAX9X        Icon name: computer-laptop          Chassis: laptop       Machine ID: fc555b04b49048fe96a26e2f5cee10dd          Boot ID: cfa91482527442b3b658109a7e429e6c Operating System: Ubuntu 22.04.1 LTS           Kernel: Linux 5.15.0-56-generic     Architecture: x86-64  Hardware Vendor: HONOR   Hardware Model: NBLK-WAX9X  18:41:33 up  5:49,  1 user,  load average: 0.71, 0.31, 0.28

--- cachesim/cache.c
+++ cachesim/cache.c
@@ -91,7 +91,7 @@ void cache_write(uintptr_t addr, uint32_t data, uint32_t wmask) {
   {
     if(((cache[i].tag == tag) & (cache[i].valid)))
     {
-      cache[i].data[group_addr] = (cache[i].data[group_addr] & (~wmask)) | (data & wmask);
+      cache[i].data[group_addr] = (data & wmask);
       cache[i].dirty_bit = 1;
       cache[i].valid = 1;
       return;
